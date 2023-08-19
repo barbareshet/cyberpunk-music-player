e52e5e391962b3e5ced39f6f01f47b6d8305dfe8
ido barnea <barbareshet@gmail.com>
2023-08-19 09:08:23 +0300

    project

---
+++ README.md
@@ -0,0 +1,9 @@
+# React + Vite
+-- This app is based on [this](https://tropicolx.hashnode.dev/creating-a-cyberpunk-themed-music-player-with-react-web-audio-api-and-canvas) example.--
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/components/SongDetails.jsx
@@ -0,0 +1,32 @@
+// components/SongDetails.jsx
+import React from "react";
+import SongImage from "./SongImage";
+import Visualizer from "./Visualizer.jsx";
+import "../styles/SongDetails.css";
+
+const SongDetails = ({ song,
+                         visualizer,
+                         source,
+                         analyser,
+                         currentSongIndex }) => {
+    return (
+        <div className="songDetails">
+            <div className="songDetails__imageContainer">
+                {!visualizer && <SongImage url={song.image} />}
+                {visualizer && (
+                    <Visualizer
+                        source={source}
+                        analyser={analyser}
+                        currentSongIndex={currentSongIndex}
+                    />
+                )}
+            </div>
+            <div className="songDetails__info">
+                <h3 className="songDetails__songName">{song.title}</h3>
+                <h4 className="songDetails__artistName">{song.artist}</h4>
+            </div>
+        </div>
+    );
+};
+
+export default SongDetails;
